5c10fee8b9771620d146cf3dc96c5f389ba6b548
AnteVuletic <antevuletic1995@gmail.com>
2019-03-18 14:18:46 +0100

    First layout prototype for mac

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,3 +5,10 @@ hamburger.addEventListener("click",(event)=>{
     navbar.classList.toggle("navbar--show");
     hamburger.classList.toggle("navigation__hamburger--clicked");
 });
+
+document.addEventListener("scroll", (event)=>{
+    if(window.pageYOffset !== 0)
+        navbar.classList.add("navbar--scrolled");
+    else
+        navbar.classList.remove("navbar--scrolled");
+});
